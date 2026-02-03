69c8c38732351c83880577903cea03fe683b1b51
Sahid Al Afzal <sahidalafzal960@gmail.com>
2026-02-04 01:01:34 +0530

    hide unnecessary files

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from .routers import auth, users, trains, bookings, payment, admin
 from sqlalchemy.orm import Session
 from app.database import SessionLocal, engine
 import os
-#models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(title="RailBay")
